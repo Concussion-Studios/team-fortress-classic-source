fa2aae912f7ade670ed3e71ee904d6922b06a6d3
FriskTheFallenHuman <nikodash2020@hotmail.com>
2020-08-04 12:42:44 -0500

    Update what i had left

--- mp/src/materialsystem/stdshaders/clean.bat
+++ mp/src/materialsystem/stdshaders/clean.bat
@@ -3,8 +3,8 @@ setlocal
 
 if /i "%1" == "-game" goto CleanGameDir
 
-rem Clean out hl2
-if exist ..\..\..\game\hl2\shaders rd /s /q ..\..\..\game\hl2\shaders
+rem Clean out tfc
+if exist ..\..\..\game\tfc\shaders rd /s /q ..\..\..\game\tfc\shaders
 goto CleanOtherStuff
 
 :CleanGameDir
@@ -14,14 +14,9 @@ if exist "%__GameDir%\shaders" rd /s /q "%2\shaders"
 goto CleanOtherStuff
 
 :CleanOtherStuff
-if exist debug_dx9 rd /s /q debug_dx9
-
-if exist fxctmp9 rd /s /q fxctmp9
-if exist vshtmp9 rd /s /q vshtmp9
-if exist pshtmp9 rd /s /q pshtmp9
-if exist fxctmp9_tmp rd /s /q fxctmp9_tmp
-if exist vshtmp9_tmp rd /s /q vshtmp9_tmp
-if exist pshtmp9_tmp rd /s /q pshtmp9_tmp
+if exist Debug_dx9_* rd /s /q Debug_dx9_*
+if exist Release_dx9_* rd /s /q Release_dx9_*
+if exist include rd /s /q include
 if exist shaders rd /s /q shaders
 goto end
 
